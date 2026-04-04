b98f57f292f89a727bc4adfa91a43811a1f263f0
rounakdutta121 <rou.nak153d@gmail.com>
2026-04-04 11:11:46 +0530

    Fix sitemap import path

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from 'next'
-import { blogs } from './data'
+import { blogs } from '../data'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://damnartusa.vercel.app'
